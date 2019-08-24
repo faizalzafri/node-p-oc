dad9e98df57baa0fca0d84fb0ec5548e1fea80e9
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-08-24 22:02:16 +0530

    models - embed

---
+++ models/movie.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const {genreSchema} = require('./genre');
+
+exports.Movie = mongoose.model('Movie', new mongoose.Schema({
+  title:{
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  genre:{
+    type: genreSchema,
+    required: true
+  },
+  numberInStock:{
+    type: Number,
+    required: true,
+    min: 0,
+    max:255
+  },
+  dailyRentalRate:{
+    type: Number,
+    required: true,
+    min: 0,
+    max:255
+  }
+}));
+
+exports.validateMovie = (movie) => {
+  const schema = {
+    title : Joi.string().min(5).max(50).required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().min(0).required(),
+    dailyRentalRate: Joi.number().min(0).required()
+  };
+  return Joi.validate(movie,schema);
+}
